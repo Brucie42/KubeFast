600c7509920c89a53d6b565cd38538b21630346a
Brucie55 <136215957+Brucie55@users.noreply.github.com>
2025-08-13 21:02:54 -0600

    Changed main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import socket
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Hello from Kubernetes via Docker Desktop!"}
+    pod_name = os.getenv("HOSTNAME", "unknown")
+    return {
+        "message": "Hello from Kubernetes!",
+        "pod": pod_name,
+        "host": socket.gethostname()
+    }
